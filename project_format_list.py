d7bd7a07d1a1cccdfddb1d1e8fa44334f3ad0825
Jeffrey Graessley <126127425+grassLEE@users.noreply.github.com>
2023-12-10 08:31:32 -0800

    Add files via upload

---
+++ project_format_list.py
@@ -0,0 +1,23 @@
+
+def get_project_info(input_prompt):
+    """
+    Function to collect project names and descriptions from a client's project list from a spreadsheet.
+    """
+    values = []
+    while True:
+        user_input = input(input_prompt + ' (press return finish):\n')
+        if not user_input:
+            break
+        values.append(user_input)
+    return values
+
+def projectlist_formatter(project_names, project_descriptions):
+    """Format lists of names/descriptions"""
+    project_list = zip(project_names, project_descriptions)
+    for project, desc in project_list:
+        print(f"\n{project}: \n{desc}")
+
+names_values = get_project_info('Enter project names')
+descriptions_values = get_project_info('Enter project descriptions (multiline)')
+
+projectlist_formatter(project_names=names_values, project_descriptions=descriptions_values)
